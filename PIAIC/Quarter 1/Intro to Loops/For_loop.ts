633480fa7406ea8a2b5c298c35ff4b19f1daf78e
Engr.Jalal Saleem <2022EE275@student.uet.edu.pk>
2023-12-16 22:06:23 +0500

    Add files via upload
    Intro To Loops

---
+++ PIAIC/Quarter 1/Intro to Loops/For_loop.ts
@@ -0,0 +1,21 @@
+let fruits: string[] = [
+  "Apple",
+  "Orange",
+  "Banana",
+  "Mango",
+  "Grapes",
+  "Watermelon",
+  "Strawberry",
+  "Kiwi",
+  "Pineapple",
+];
+for (let index = 0; index < fruits.length; index++) {
+    const element = fruits[index];
+    console.log(`${index+1} fruit: ${element}`);
+};
+
+//give me an array of 100 fruits
+
+
+
+export{};
