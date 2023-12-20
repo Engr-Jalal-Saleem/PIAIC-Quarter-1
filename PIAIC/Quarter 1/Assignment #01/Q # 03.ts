4f98781cadc8c378abcb997943f812e6c4ddf69c
Engr-Jalal-Saleem <124458089+Engr-Jalal-Saleem@users.noreply.github.com>
2023-12-21 00:25:30 +0500

    Bug Fixed

--- PIAIC/Quarter 1/Assignment #01/Q # 03.ts
+++ PIAIC/Quarter 1/Assignment #01/Q # 03.ts
@@ -1,3 +1,4 @@
 let famousPerson : string = "Ibn - e - Batuta";
 let message : string = "The world is a book, and those who do not travel read only a page.";
 console.log(`${famousPerson} once said, ${message}`);
+export{}
